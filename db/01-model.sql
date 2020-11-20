0ca0309779e4b837f24867145c03a0862ead8f9e
MrBasch <mikhail.baschsmanna@utem.cl>
2020-11-20 19:04:45 -0300

    mejora en el modelo 01

--- db/01-model.sql
+++ db/01-model.sql
@@ -9,7 +9,6 @@ CREATE TABLE pais(
     currencyName varchar(255), -- nombre de la moneda
     Lang varchar(255), -- codigo iso 4217 nombre moneda nacional
     Name varchar(255), -- nombre del pais 
-    Region varchar(255), -- region a la cual pertenece el pais 
     PRIMARY KEY (pk)
 );
 CREATE UNIQUE INDEX ON pais(UPPER(TRIM(both FROM codeAlpha2)));
@@ -19,7 +18,7 @@ CREATE UNIQUE INDEX ON pais(UPPER(TRIM(both FROM Region)));
 CREATE UNIQUE INDEX ON pais(UPPER(TRIM(both FROM Lang)));
 CREATE UNIQUE INDEX ON pais(UPPER(TRIM(both FROM currencyCode)));
 
-DROP TABLE IF EXISTS indicador CASCADE;
+DROP TABLE IF EXISTS indicadores CASCADE;
 CREATE TABLE indicadores(
     pk bigserial NOT NULL, --Autoincremental
     Name varchar(255), -- nombre del indicador
@@ -28,7 +27,7 @@ CREATE TABLE indicadores(
     PRIMARY KEY (pk)
 );
 
-DROP TABLE IF EXISTS periodo CASCADE;
+DROP TABLE IF EXISTS periodos CASCADE;
 CREATE TABLE periodos(
     pk bigserial NOT NULL, --Autoincremental
     año int NOT NULL,  -- periodo de los datos (año) 
@@ -41,17 +40,17 @@ CREATE TABLE fact(
     fk_periodo bigint NOT NULL, --referencia a la tabla periodo, fecha de los datos 
     fk_pais bigint NOT NULL, --referencia al pais al cual le pertenecen los datos 
     fk_indicadores bigint NOT NULL, -- referencia a los datos de los indicadores 
-    PIB int, -- Producto interno bruto 
-    TDA int,  --Tasa de desempleo anual
-    IFL int,  -- Inflacion
-    IVA int,  --Impuesto valor agregado
-    PRF int,  -- Presion fiscal 
-    SMI int,  -- Salario minimo
-    TSC int, -- Tasa de cambio US$ a actuales 
-    DBI int,  -- Doing business index (indice de facilidad para hacer negocios)
-    FOREIGN KEY (fk_periodo) REFERENCES periodo(pk) ON UPDATE CASCADE ON DELETE CASCADE, 
+    PIB double precision, -- Producto interno bruto 
+    TDA double precision,  --Tasa de desempleo anual
+    IFL double precision,  -- Inflacion
+    IVA double precision,  --Impuesto valor agregado
+    PRF double precision,  -- Presion fiscal 
+    SMI double precision,  -- Salario minimo
+    TSC double precision, -- Tasa de cambio US$ a actuales 
+    DBI double precision,  -- Doing business index (indice de facilidad para hacer negocios)
+    FOREIGN KEY (fk_periodo) REFERENCES periodos(pk) ON UPDATE CASCADE ON DELETE CASCADE, 
     FOREIGN KEY (fk_pais) REFERENCES pais(pk) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (fk_indicadores) REFERENCES indicador(pk) ON UPDATE CASCADE ON DELETE CASCADE, 
+    FOREIGN KEY (fk_indicadores) REFERENCES indicadores(pk) ON UPDATE CASCADE ON DELETE CASCADE, 
      PRIMARY KEY (pk)
 );
 COMMIT;
